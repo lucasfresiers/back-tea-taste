6418df51bdc83ecbd2cf2e49ca999aa05debf7b6
lucasfresiers <fresiers.lucas@gmail.com>
2023-03-13 14:36:03 +0000

    Create 3 new tables

---
+++ src/main/resources/db/changelog/003-create-table-variete.sql
@@ -0,0 +1,10 @@
+CREATE TABLE t_variete(
+   id  SERIAL NOT NULL PRIMARY KEY
+  ,nom VARCHAR(255) NOT NULL
+);
+INSERT INTO t_variete(nom) VALUES ('Thé noir');
+INSERT INTO t_variete(nom) VALUES ('Thé vert');
+INSERT INTO t_variete(nom) VALUES ('Thé blanc');
+INSERT INTO t_variete(nom) VALUES ('Thé sombre');
+INSERT INTO t_variete(nom) VALUES ('Roobios');
+INSERT INTO t_variete(nom) VALUES ('Oolong');
